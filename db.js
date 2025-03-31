1e9c8b1334db75386afc53d68e480cd8ef6bd201
Animesh Khare <khareanimesh6@gmail.com>
2025-03-31 19:01:41 +0530

    Updated MySQL connection for Railway

--- db.js
+++ db.js
@@ -1,14 +1,14 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'ballast.proxy.rlwy.net',
-    user: 'root',
-    password: 'sJCuASsxhGJyTgydjvZVYhFOJrgglcXE',
-    database: 'railway',
-    port: 32436,
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
-});
+  });
 
 module.exports = pool.promise();
